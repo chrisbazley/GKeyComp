chrisbazley/GKeyComp

commit c059185924e78e6f940c00e1341eb12f018baab0

Dogfooding the _Optional qualifier

gkcommon.c
@@ -82,9 +82,12 @@ static bool fcopy(FILE *in, FILE *out)
   return success;
 }
 
-static bool process_file(const char *input_file, const char *output_file, GKProcessFn *processor, unsigned int history_log_2, bool verbose, bool time, bool compress)
+static bool process_file(_Optional const char *input_file,
+                         _Optional const char *output_file,
+                         GKProcessFn *processor, unsigned int history_log_2,
+                         bool verbose, bool time, bool compress)
 {
-  FILE *out = NULL, *in = NULL, *tmp = NULL;
+  _Optional FILE *out = NULL, *in = NULL, *tmp = NULL;
   bool success = true;
 
   if (input_file != NULL) {
@@ -92,7 +95,7 @@ static bool process_file(const char *input_file, const char *output_file, GKProc
     if (verbose)
       printf("Opening input file '%s'\n", input_file);
 
-    in = fopen(input_file, "rb");
+    in = fopen(&*input_file, "rb");
     if (in == NULL) {
       fprintf(stderr, "Failed to open input file: %s\n", strerror(errno));
       success = false;
@@ -105,7 +108,7 @@ static bool process_file(const char *input_file, const char *output_file, GKProc
 
   if (success) {
     if (output_file != NULL) {
-      if (input_file != NULL && strcmp(output_file, input_file) == 0) {
+      if (input_file != NULL && strcmp(&*output_file, &*input_file) == 0) {
         /* Can't overwrite the input file whilst reading from it, so direct
            output to a temporary file instead */
         if (verbose)
@@ -122,7 +125,7 @@ static bool process_file(const char *input_file, const char *output_file, GKProc
         if (verbose)
           printf("Opening output file '%s'\n", output_file);
 
-        out = fopen(output_file, "wb");
+        out = fopen(&*output_file, "wb");
         if (out == NULL) {
           fprintf(stderr, "Failed to open output file: %s\n", strerror(errno));
           success = false;
@@ -134,10 +137,10 @@ static bool process_file(const char *input_file, const char *output_file, GKProc
     }
   }
 
-  if (success) {
+  if (success && in && out) {
     const clock_t start_time = time ? clock() : 0;
 
-    success = processor(in, tmp != NULL ? tmp : out, history_log_2, verbose);
+    success = processor(&*in, tmp != NULL ? &*tmp : &*out, history_log_2, verbose);
 
     if (success && time)
     {
@@ -149,7 +152,7 @@ static bool process_file(const char *input_file, const char *output_file, GKProc
   if (in != NULL && in != stdout) {
     if (verbose)
       puts("Closing input file");
-    fclose(in);
+    fclose(&*in);
   }
 
   /* If we wrote to a temporary file then copy it to the real output */
@@ -160,7 +163,7 @@ static bool process_file(const char *input_file, const char *output_file, GKProc
         if (verbose)
           printf("Opening output file '%s'\n", output_file);
 
-        out = fopen(input_file, "wb");
+        out = fopen(&*input_file, "wb");
         if (out == NULL) {
           fprintf(stderr,
                   "Failed to open output file: %s\n",
@@ -171,14 +174,16 @@ static bool process_file(const char *input_file, const char *output_file, GKProc
         /* Default output is to standard output stream */
         out = stdout;
       }
+    }
 
+    if (success) {
       if (verbose)
         puts("Copying from temporary to final output");
 
-      if (fseek(tmp, 0L, SEEK_SET)) {
+      if (fseek(&*tmp, 0L, SEEK_SET)) {
         fprintf(stderr, "Failed to seek start of temporary file\n");
         success = false;
-      } else if (!fcopy(tmp, out)) {
+      } else if (!fcopy(&*tmp, &*out)) {
         success = false;
       }
     }
@@ -186,13 +191,13 @@ static bool process_file(const char *input_file, const char *output_file, GKProc
     /* Close the temporary file (which also deletes it) */
     if (verbose)
       puts("Closing temporary file");
-    fclose(tmp);
+    fclose(&*tmp);
   }
 
   if (out != NULL && out != stdout) {
     if (verbose)
       puts("Closing output file");
-    if (fclose(out)) {
+    if (fclose(&*out)) {
       fprintf(stderr, "Failed to close output file: %s\n", strerror(errno));
       success = false;
     }
@@ -205,7 +210,7 @@ static bool process_file(const char *input_file, const char *output_file, GKProc
       if (verbose)
         puts("Setting type of output file");
 
-      if (!set_file_type(output_file, compress)) {
+      if (!set_file_type(&*output_file, compress)) {
         fputs("Failed to set output file type\n", stderr);
         success = false;
       }
@@ -214,7 +219,7 @@ static bool process_file(const char *input_file, const char *output_file, GKProc
     /* Delete malformed output unless debugging is enabled or
        it may actually be the input (still intact) */
     if (!success && !verbose && out != NULL && out != stdout)
-      remove(output_file);
+      remove(&*output_file);
   }
 
   return success;
@@ -258,12 +263,12 @@ int main_common(int argc, const char *argv[], GKProcessFn *processor, const char
   int n;
   bool verbose = false, time = false, batch = false;
   int rtn = EXIT_SUCCESS;
-  const char *output_file = NULL, *input_file = NULL;
+  _Optional const char *output_file = NULL, *input_file = NULL;
   unsigned int history_log_2 = FEDNET_COMP_LOG_2;
 
   assert(argc > 0);
   assert(argv != NULL);
-  assert(processor != NULL);
+  assert(processor);
   assert(description != NULL);
 
 #ifdef FORTIFY

gkcomp.c
@@ -52,7 +52,7 @@ static void show_progress(long int in, long int out)
            ((double)out * 100) / in, in, out);
 }
 
-static bool update_progress(void *arg, size_t in, size_t out)
+static bool update_progress(_Optional void *arg, size_t in, size_t out)
 {
   NOT_USED(arg);
 
@@ -90,7 +90,7 @@ static bool comp(FILE *in, FILE *out, unsigned int history_log_2, bool verbose)
   char in_buffer[BUFFER_SIZE], out_buffer[BUFFER_SIZE];
   bool success = false;
   long int in_total, out_total, in_told;
-  GKeyComp *comp = NULL;
+  _Optional GKeyComp *comp = NULL;
   GKeyStatus status;
   GKeyParameters params;
 
@@ -142,7 +142,7 @@ static bool comp(FILE *in, FILE *out, unsigned int history_log_2, bool verbose)
   params.out_buffer = out_buffer;
   params.out_size = sizeof(out_buffer);
   params.in_size = 0;
-  params.prog_cb = verbose ? update_progress : NULL;
+  params.prog_cb = verbose ? update_progress : (GKeyProgressFn *)NULL;
   params.cb_arg = NULL;
 
   do {
@@ -168,7 +168,7 @@ static bool comp(FILE *in, FILE *out, unsigned int history_log_2, bool verbose)
     /* Compress the data from the input buffer to the output buffer.
        If the input buffer is empty then this flushes any pending output.
        Returns GKeyStatus_Finished when the flush is complete. */
-    status = gkeycomp_compress(comp, &params);
+    status = gkeycomp_compress(&*comp, &params);
 
     /* Is the output buffer full or have we finished? */
     if (status == GKeyStatus_Finished ||

gkdecomp.c
@@ -55,7 +55,7 @@ static void show_progress(long int in, long int out)
   }
 }
 
-static bool update_progress(void *arg, size_t in, size_t out)
+static bool update_progress(_Optional void *arg, size_t in, size_t out)
 {
   NOT_USED(arg);
 
@@ -71,7 +71,7 @@ static bool decomp(FILE *in, FILE *out, unsigned int history_log_2, bool verbose
   char in_buffer[BUFFER_SIZE], out_buffer[BUFFER_SIZE];
   bool in_pending, success = false;
   long int expected, out_total, in_total;
-  GKeyDecomp *decomp = NULL;
+  _Optional GKeyDecomp *decomp = NULL;
   GKeyStatus status;
   GKeyParameters params;
 
@@ -110,7 +110,7 @@ static bool decomp(FILE *in, FILE *out, unsigned int history_log_2, bool verbose
   params.out_buffer = out_buffer;
   params.out_size = sizeof(out_buffer);
   params.in_size = 0;
-  params.prog_cb = verbose ? update_progress : NULL;
+  params.prog_cb = verbose ? update_progress : (GKeyProgressFn *)NULL;
   params.cb_arg = NULL;
 
   do {
@@ -130,7 +130,7 @@ static bool decomp(FILE *in, FILE *out, unsigned int history_log_2, bool verbose
     }
 
     /* Decompress the data from the input buffer to the output buffer */
-    status = gkeydecomp_decompress(decomp, &params);
+    status = gkeydecomp_decompress(&*decomp, &params);
 
     /* If the input buffer is empty and it cannot be (re-)filled then
        there is no more input pending. */

misc.h
@@ -42,4 +42,21 @@
 
 #endif /* USE_CBDEBUG */
 
+#ifdef USE_OPTIONAL
+#include <stdio.h>
+
+#undef NULL
+#define NULL ((_Optional void *)0)
+
+static inline _Optional FILE *optional_fopen(const char *name, const char *mode)
+{
+    return fopen(name, mode);
+}
+#undef fopen
+#define fopen(p, n) optional_fopen(p, n)
+
+#else
+#define _Optional
+#endif
+
 #endif /* MISC_H */

version.h
@@ -7,6 +7,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_STRING "0.05 [19 May 2024]"
+#define VERSION_STRING "0.06 [13 Apr 2025]"
 
 #endif /* VERSION_H */

commit 482551c17437b89b2e5c75d86d33a8ee82c8fcb4

Fix a bug introduced by c0591859 which broke batch mode because out can be null in process_file

gkcommon.c
@@ -133,11 +133,13 @@ static bool process_file(_Optional const char *input_file,
     }
   }
 
-  if (success && in && out) {
+  _Optional FILE *imm_out = (tmp != NULL ? tmp : out);
+
+  if (success && in && imm_out) {
     const clock_t start_time = time ? clock() : 0;
 
     success =
-      processor(&*in, tmp != NULL ? &*tmp : &*out, history_log_2, verbose);
+      processor(&*in, &*imm_out, history_log_2, verbose);
 
     if (success && time) {
       printf("Time taken: %.2f seconds\n",